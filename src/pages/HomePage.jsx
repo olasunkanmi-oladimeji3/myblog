f67da4553f9386dcd1a232dbee48ce075cc8256f
olasunkanmi-oladimeji3 <oladimeji.olasunkanmi321@gmail.com>
2025-04-29 13:09:18 +0100

    3 card box

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,11 @@ function HomePage(params) {
     return(
         <>
             <h1>Home Page</h1>
+            <div className="display-grid">
+                <div className="card"></div>
+                <div className="card"></div>
+                <div className="card"></div>
+            </div>
         </>
     )
     
